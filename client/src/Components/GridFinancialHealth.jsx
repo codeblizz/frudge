29c981e7e6e7a63868de4c5e83736e8b3da68d95
codeblizz <codeblizzen@gmail.com>
2021-09-08 21:53:03 +0400

    Finito

--- client/src/Components/GridFinancialHealth.jsx
+++ client/src/Components/GridFinancialHealth.jsx
@@ -39,13 +39,28 @@ const GridFinancialHealth = () => {
                 <b className="col-8">My Financial Health</b>
                 <small className="d-flex justify-content-end mx-1 col border bg-goal-pill rounded-pill">View Financial Health</small>
             </div>
-            <div className="d-flex p-2 card-stats h-20">
-                <div class="card-body">
-                    {/* <Scatter ref={chartRef} data={data} /> */}
-                    <div className="footer-goal">
+            <div className="d-flex p-2 card-stats row justify-content-center align-item-end">
+                <div className="card-body col">
+                    <div className="row">
+                        <div className="progress" style={{height: "5px"}}>
+                            <div className="progress-bar bg-danger" role="progressbar" style={{width: "40%"}} aria-valuenow="0" aria-valuemin="0" aria-valuemax="30"></div>
+                            <div className="progress-bar bg-warning" role="progressbar" style={{width: "70%"}} aria-valuenow="30" aria-valuemin="30" aria-valuemax="60"></div>
+                            <div className="progress-bar bg-success" role="progressbar" style={{width: "100%"}} aria-valuenow="60" aria-valuemin="60" aria-valuemax="100"></div>
+                        </div>
+                        <span className="row justify-content-start">
+                            <small className="d-flex col justify-content-start text-danger text-goal">POOR</small>
+                            <small className="d-flex col justify-content-center text-warning text-goal ">AVERAGE</small>
+                            <small className="d-flex col justify-content-end text-success text-goal">GOOD</small>
+                        </span>
+                    </div>
+                    <div className="footer-goal row">
                         <hr/>
-                        <div className="row">
-                            <small className="d-flex text-goal justify-content-center col">Yuor financial health status is {" "}<p className="text-success">Good</p></small>
+                        <div className="col">
+                            <small className="health-goal col">Yuor financial health status is 
+                                <span className="text-success good-health">Good.</span>
+                                However there's still room for improvement.
+                                <span className="good-health text-primary">See how you can improve</span>
+                            </small>
                         </div>
                     </div>
                 </div>
