f7db779d19e17ac885b10c6807f488ed9cdaffcb
codeblizz <codeblizzen@gmail.com>
2021-09-08 18:09:25 +0400

    Circular progressbar

--- client/src/Components/Goal.jsx
+++ client/src/Components/Goal.jsx
@@ -1,19 +1,11 @@
-import React, { useRef } from 'react'
-import { Pie } from "react-chartjs-2";
-import { datasetKeyProvider, statusColor, status } from "../constants";
+import React from 'react'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const Goal = () => {
-    const chartRef = useRef(null);
-
-    const pieData = {
-        labels: [...Object.keys(status)],
-        datasets: [
-          {
-            backgroundColor: Object.keys(status).map(s => statusColor[s]),
-            data: [...Object.values(status)]
-          }
-        ]
-    };
+    const percentage1 = 66;
+    const percentage2 = 78;
+    const percentage3 = 46;
 
     return (
         <div className="col">
@@ -21,17 +13,50 @@ const Goal = () => {
             <div className="card-group row g-4">
                 <div className="col p-2">
                     <div className="card-body goalCard">
-                        <Pie width="10px" ref={chartRef} data={pieData} datasetKeyProvider={datasetKeyProvider} />
+                        <p className="col d-flex justify-content-center">Retirement</p>
+                        <CircularProgressbar 
+                            width="10px" 
+                            value={percentage1} 
+                            text={`${percentage1}%`} 
+                            className="c_progress"
+                            styles={buildStyles({ 
+                                pathColor: `rgba(138, 165, 62, ${percentage1 / 100})`,
+                                textColor: '#333330',
+                                trailColor: '#d6d6d6',
+                            })}
+                        />
                     </div>
                 </div>
                 <div className="col p-2">
                     <div className="card-body goalCard">
-                        <Pie width="10px" ref={chartRef} data={pieData} datasetKeyProvider={datasetKeyProvider} />
+                        <p className="col d-flex justify-content-center">Vacation Home</p>
+                        <CircularProgressbar 
+                            width="10px" 
+                            value={percentage2} 
+                            text={`${percentage2}%`} 
+                            className="c_progress"
+                            styles={buildStyles({ 
+                                pathColor: `rgba(138, 165, 62, ${percentage2 / 100})`,
+                                textColor: '#333330',
+                                trailColor: '#d6d6d6',
+                            })}
+                        />
                     </div>
                 </div>
                 <div className="col p-2">
                     <div className="card-body goalCard">
-                        <Pie width="10px" ref={chartRef} data={pieData} datasetKeyProvider={datasetKeyProvider} />
+                        <p className="col d-flex justify-content-center">Travel</p>
+                        <CircularProgressbar 
+                            width="10px" 
+                            value={percentage3} 
+                            text={`${percentage3}%`} 
+                            className="c_progress"
+                            styles={buildStyles({ 
+                                pathColor: `rgba(138, 165, 62, ${percentage3 / 100})`,
+                                textColor: '#333330',
+                                trailColor: '#d6d6d6',
+                            })}
+                        />
                     </div>
                 </div>
             </div>
