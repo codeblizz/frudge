fdbe39993912798887ec7fc6b06b663e2af96148
codeblizz <codeblizzen@gmail.com>
2021-09-09 15:46:24 +0400

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'client', 'build', "index.html")))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')) 
-})
-
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, 'client', 'build', "index.html")))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')) 
+    })
+}
 app.listen(port, () => {   
     console.log(`server running on port ${port}`);
 });
